8f34f7190efc32d36829357ed709c4c4bef25bcf
Syed <mirulkimi226@gmail.com>
2025-02-11 09:36:30 +0800

    DarkMode : serius x de bug

--- app/(main)/learn/components/Navbar.tsx
+++ app/(main)/learn/components/Navbar.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Navbar = ({ title }: Props) => {
   return (
-    <nav className="sticky top-0 text-[color:var(--foreground)] pb-3 lg:pt-7 lg:-mt-7 flex items-center justify-between border-b-2 mb-5 text-[color:var(--foreground)] lg:z-50">
+    <nav className="sticky top-0 bg-white dark:bg-[#404040] pb-3 lg:pt-7 lg:-mt-7 flex items-center justify-between border-b-2 mb-5 text-[color:var(--foreground)] lg:z-50">
       <Link href="/courses">
         <Button variant="secondary" size="sm">
           <ChevronLeft className="h-5 w-5 stroke-2 text-neutral-400" />
