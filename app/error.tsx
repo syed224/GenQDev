570ab5f9c4127043c2525cfd48c4cc5cb2c29cbc
Syed <mirulkimi226@gmail.com>
2025-02-11 16:22:10 +0800

    Update: enhance dark mode support in Navbar, error, and not-found components

--- app/error.tsx
+++ app/error.tsx
@@ -4,8 +4,8 @@ import { Button } from "@/components/ui/button";
 
 const HomePageError = ({ err, reset }: { err: Error; reset: () => void }) => {
   return (
-    <div className="w-screen h-screen flex flex-col gap-y-4 justify-center items-center fixed top-0 left-0 z-[10000] bg-white/55 backdrop-blur-sm">
-      <h1 className="text-2xl font-thin text-dark-400 text-center">
+    <div className="w-screen h-screen flex flex-col gap-y-4 justify-center items-center fixed top-0 left-0 z-[10000] bg-white/55 dark:bg-[#404040] backdrop-blur-sm">
+      <h1 className="text-2xl font-thin text-[color(--foreground)] text-center">
         {err?.message || "Something went wrong !"}
       </h1>
       <div className="flex gap-x-4 items-center">
